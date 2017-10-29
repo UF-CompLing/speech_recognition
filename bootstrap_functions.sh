fc6016545181393916b4edf881724717d4bb81aa
Morgan Wixted <morgan@Morgans-MacBook-Pro.local>
2017-10-29 01:55:09 -0400

    fix to sudo apt

--- bootstrap_functions.sh
+++ bootstrap_functions.sh
@@ -1,8 +1,8 @@
 function install_theano() {
 	#begin installing theano dependencies using pip
 	echo "INSTALLING: python pip, numpy, scipy, openBLAS"
-	install python-pip
+	sudo apt install python-pip
 	pip install numpy
 	pip install scipy
-	sudo apt-get install libopenblas-dev
+	sudo apt install libopenblas-dev
 }
